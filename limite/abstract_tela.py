6309fe456fdf339541aac91e3a9a1f819702495c
TiagoSansao <tiagossansao@gmail.com>
2025-05-25 11:40:12 -0300

    feat(abstract_tela): abstracao para ler data

--- limite/abstract_tela.py
+++ limite/abstract_tela.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import datetime
 
 
 class AbstractTela(ABC):
@@ -26,6 +27,15 @@ class AbstractTela(ABC):
                 return valor_lido
             except ValueError:
                 print("Valor incorreto! Somente números")
+
+    def le_input_data(self, mensagem):
+        while True:
+            valor_lido = input(mensagem)
+            try:
+                data = datetime.datetime.strptime(valor_lido, "%d-%m-%Y")
+                return data
+            except ValueError:
+                print("Valor incorreto! Formato esperado: DD-MM-YYYY")
 
     @abstractmethod
     def tela_opcoes(self):
