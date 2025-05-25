15c07e72df89cc401a4b1104d1722f8e0a14457a
TiagoSansao <tiagossansao@gmail.com>
2025-05-25 11:07:38 -0300

    fix(tela_hotel): atualizar opcoes validas

--- limite/tela_hotel.py
+++ limite/tela_hotel.py
@@ -11,7 +11,8 @@ class TelaHotel(AbstractTela):
         print("3 - Listar Hoteis")
         print("4 - Excluir Hoteis")
         print("0 - Retornar")
-        opcao = super().le_input_int("Opção escohida: ", [0,1,2,3,4,5])
+        
+        opcao = super().le_input_int("Opção escohida: ", [0,1,2,3,4])
         return opcao
 
     def pega_dados_hotel(self):
